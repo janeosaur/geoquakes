fdcd343ed29647cc50d3d25e1db22afbcfb0bf9a
janeosaur <janewie@gmail.com>
2017-04-05 23:00:32 -0700

    pop and reverse latlong arrayy

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,32 @@
 // define globals
-var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var weekly_quakes_endpoint = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
 
 $(document).on("ready", function() {
-   $.ajax( {
+  $.ajax({
     method: "GET",
-    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson" , 
+    url: weekly_quakes_endpoint ,
     dataType: "json",
     success: onSuccess
   });   // ajax
+
+  var sfLatLng = {lat: 37.78, lng: -122.44};
+  function initMap() {
+  	var map = new google.maps.Map(document.getElementById('map'), {
+  	  center: sfLatLng,
+  	  zoom: 8
+    });
+   	var marker = new google.maps.Marker({
+        position: sfLatLng,
+        map: map,
+        title: 'Hello World!'
+  	  });   // marker
+    };
+
+    google.maps.event.addDomListener(window, 'load', initMap);
+
 });  // doc on ready
+
+
   function onSuccess(responseData) {
 
     var output = responseData.features;
@@ -16,24 +34,36 @@ $(document).on("ready", function() {
     	var newOutput = output[index].properties.title;
     	$("#info").append('<p>' + newOutput + '</p>');
     	var findLatLng = output[index].geometry.coordinates;
-    	console.log(findLatLng);
-    })
-    // [0].properties.title;
-    // for each (var in output) {
-}; 
+    	// console.log(findLatLng); // to test
 
-var sfLatLng = {lat: 37.78, lng: -122.44};
-function initMap() {
-	var map = new google.maps.Map(document.getElementById('map'), {
-	  center: sfLatLng,
-	  zoom: 8
-	});  // google.maps
+      findLatLng.pop();
+      findLatLng.reverse(); // turn this into object and feed into maps
+      console.log(findLatLng); // to test
 
- 	var marker = new google.maps.Marker({
-      position: sfLatLng,
-      map: map,
-      title: 'Hello World!'
-	});   // marker
-}    // initMap
-google.maps.event.addDomListener(window, 'load', initMap);
+})};
+
+
+// greyed for now
+
+
+    //   var marker = new google.maps.Marker({
+    //       position: findLatLng,
+    //       map: map,
+    //       title: 'Hello World!'
+    //     });   // marker
+    //
+    //   var mapOptions = {
+    //     zoom: 4,
+    //     center: findLatLng
+    //   };
+    //
+    //   marker.setMap(map);
+    //
+    //   google.maps.event.addDomListener(window, 'load', mapOptions);
+    // })};
+
+
+
+
+
 // }
