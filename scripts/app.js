29a4dc518c11ffeb70f8d9853a72b151211f08c9
janeosaur <janewie@gmail.com>
2017-04-05 23:59:15 -0700

    multiple pins are showing up! base ex completed

--- scripts/app.js
+++ scripts/app.js
@@ -10,58 +10,42 @@ $(document).on("ready", function() {
   });   // ajax
 });  // doc on ready
 
+function onSuccess(responseData) {
+  var coord = [{lat: 37.78, lng: -122.44}]; // creates array starting with SF coord
+
+  var output = responseData.features;
+
+  output.forEach(function(item, index) {
+    var newOutput = output[index].properties.title;
+    $("#info").append('<p>' + newOutput + '</p>');
+    var findLatLng = output[index].geometry.coordinates;
+    // console.log(findLatLng); // to test
+
+    findLatLng.pop(); // remove Depth
+    findLatLng.reverse(); // need turn this into object and feed into maps
+    // console.log(findLatLng); // to test
+
+    var coordObj = {}; // to turn it into object
+      coordObj['lat'] = findLatLng[0];
+      coordObj['lng'] = findLatLng[1];
+      coord.push(coordObj);
+      // console.log(coord); // test
+    })
+    google.maps.event.addDomListener(window, 'load', initMap(coord));
+  }
+
 var sfLatLng = {lat: 37.78, lng: -122.44};
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: sfLatLng,
-    zoom: 2
-  });
-  var marker = new google.maps.Marker({
-      position: sfLatLng,
+
+function initMap(coord) {
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  center: sfLatLng,
+    zoom: 1,
+	});
+
+	for (i =0; i < coord.length; i ++) {
+	 	var marker = new google.maps.Marker({
+      position: coord[i],
       map: map,
-      title: 'Hello World!'
-    });   // marker
-  var newmarker = new google.maps.Marker({
-      position: {lat: 13.6183, lng: 121.0218},
-      map: map,
-    });
-  };
-
-  google.maps.event.addDomListener(window, 'load', initMap);
-
-
-  function onSuccess(responseData) {
-
-    var output = responseData.features;
-    output.forEach(function(item, index) {
-    	var newOutput = output[index].properties.title;
-    	$("#info").append('<p>' + newOutput + '</p>');
-    	var findLatLng = output[index].geometry.coordinates;
-    	// console.log(findLatLng); // to test
-
-      findLatLng.pop();
-      findLatLng.reverse(); // need turn this into object and feed into maps
-      // console.log(findLatLng); // to test
-
-      var coordObj = {}; // to turn it into object
-        coordObj['lat'] = findLatLng[0];
-        coordObj['lng'] = findLatLng[1];
-        console.log(coordObj); // test
-      })}
-      // newMap();
-
-      // function newMap() {
-      //       // var myLatlng = new google.maps.LatLng(findLatLng[0], findLatLng[1]); this might get rid of need to turn into object
-      //       // var myLatlng = coordObj;
-      //
-      //       // var map = new google.maps.Map(document.getElementById('map'), {
-      //       //           zoom: 2,
-      //       //           center: coordObj
-      //       //         });
-      //
-      //       var newmarker = new google.maps.Marker({
-      //                 position: coordObj,
-      //                 map: map,
-      //               });
-      //           newmarker.setMap();
-      //     }})};
+		});
+	}
+}    
